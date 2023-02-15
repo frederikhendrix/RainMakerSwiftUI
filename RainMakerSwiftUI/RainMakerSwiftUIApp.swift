e735a5ab716b76875910e8aa419826ccf74eb8d4
FrederikHendrix <rikhendrix@live.nl>
2023-02-15 09:12:33 +0100

    Initial Commit

---
+++ RainMakerSwiftUI/RainMakerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RainMakerSwiftUIApp.swift
+//  RainMakerSwiftUI
+//
+//  Created by Rik Hendrix on 15/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct RainMakerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
